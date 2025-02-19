375fbd844918bb2027f092240e80be277ab4c46e
Thiago Kleber <thiago.kleber@hotmail.com>
2025-02-19 15:24:16 -0300

    Finalizado a primeira versão

---
+++ MoneyExtension.Tests/MoneyExtensionTests.cs
@@ -0,0 +1,14 @@
+﻿namespace MoneyExtension.Tests;
+
+[TestClass]
+public sealed class MoneyExtensionTests
+{
+    [TestMethod]
+    public void ShoudConvertDecimalToInt()
+    {
+        decimal valor = 279.98M;
+        var cents = valor.ToCents();
+
+        Assert.AreEqual(27998, cents);
+    }
+}
